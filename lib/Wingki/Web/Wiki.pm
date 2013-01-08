8bcbf538f0315ae8de1d6f625d9beffaa23d979f
Colin Kuskie <colink@perldreamer.com>
2013-01-07 20:03:52 -0800

    Fix editing an existing wiki page.

--- lib/Wingki/Web/Wiki.pm
+++ lib/Wingki/Web/Wiki.pm
@@ -46,7 +46,17 @@ put '/wiki/:id' => sub {
     my $current_user = get_user_by_session_id();
     my $object = fetch_object('Wiki');
     $object->can_use($current_user);
-    $object->update({param()});
+    my %params = params;
+    eval {
+        $object->verify_posted_params(\%params, $current_user);
+    };
+    if (hug) {
+        return redirect '/?error_message='.bleep;
+    }
+    else {
+        $object->update;
+        return redirect '/wiki/'.$object->uri_part.'?success_message=Updated succssfully.';
+    }
     return redirect '/wiki/'.$object->uri_part;
 };
 
